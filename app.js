1138954b9ffafed2cdfdc721778ca16c8c7170a4
maheshbasnet089 <basnetmanish089@gmail.com>
2023-08-14 21:38:36 +0545

    add login

--- app.js
+++ app.js
@@ -27,7 +27,48 @@ app.post("/createUser", async (req, res) => {
     password: bcrypt.hashSync(req.body.password, 10),
   });
   // finally create vayepaxi
-  res.send("User registered sucessfully");
+  res.redirect("/login");
+});
+
+// login User
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// LOGIN user post API
+app.post("/login", async (req, res) => {
+  // email , password
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //aako email registered xa ki xainw check garnu paryo
+  const userFound = await users.findAll({
+    where: {
+      email: email,
+    },
+  });
+
+  // if registered xainw vaney(no)
+  if (userFound.length == 0) {
+    // error faldinu paryo invalid email or email not registered error
+    res.send("Invalid email or password");
+  } else {
+    const databasePassword = userFound[0].password; // database pahila register garda ko password
+    //if registered xa vaney (yes)
+
+    // if yes(xa) vaney ,password check garnu paryo
+    const isPasswordCorrect = bcrypt.compareSync(password, databasePassword);
+    
+
+    // match vayena (no) , error->invalid password
+    if(isPasswordCorrect){
+      res.send("Login Sucessfull")
+    }else{
+      res.send("Invalid email or password")
+    }
+  }
+
+  // match vayo(yes),login sucessfully
 });
 
 // listening the server
